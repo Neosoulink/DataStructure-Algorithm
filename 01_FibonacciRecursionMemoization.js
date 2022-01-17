f3e6bacd61c9855cdf4674a42a4c8b2db083ea03
Neosoulink <neodarksoulink@gmail.com>
2022-01-17 09:20:48 +0100

    Simple fibonacci recursion with memoization

---
+++ 01_FibonacciRecursionMemoization.js
@@ -0,0 +1,18 @@
+/**
+ * Simple fibonacci recursion with memoization
+ *
+ * Optimization of 00_FibonacciRecursion.js
+ *
+ * @param {number} n
+ * @returns {number} return the fibonacci number
+ */
+function fib(n, memo = {}) {
+	if (n in memo) return memo[n];
+	if (n <= 2) return 1;
+
+	memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+	return memo[n];
+}
+
+console.log(fib(9));
+console.log(fib(50));
